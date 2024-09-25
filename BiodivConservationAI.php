446537b6a290cb76d438e4a5b2082b23c9313869
Helen Chappell <helen.chappell@durham.ac.uk>
2024-09-25 13:22:03 +0100

    Change curl timeout from 30 to 60s

--- BiodivConservationAI.php
+++ BiodivConservationAI.php
@@ -100,7 +100,7 @@ class BiodivConservationAI {
 				CURLOPT_RETURNTRANSFER => true,
 				CURLOPT_ENCODING => "",
 				CURLOPT_MAXREDIRS => 10,
-				CURLOPT_TIMEOUT => 30,
+				CURLOPT_TIMEOUT => 60,
 				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 				CURLOPT_CUSTOMREQUEST => "POST",
 				//CURLOPT_POSTFIELDS => "{\"uploaddata\":[".$this->email.",".$this->model.",".$this->project.",".$this->siteId.",\"Yes\",".$this->authkey.",".$urls."]}",
